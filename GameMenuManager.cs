84b2dfaf0c99bfbc13ce883952e9c22c9a76dab7
Sam Keene <sam@zoetrope.com.au>
2018-07-28 12:56:25 -0700

    Test
    Test

---
+++ GameMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+namespace Chapter4Recipe4 {
+public class GameMenuManager : MonoBehaviour {
+
+		public GameObject gamePlay;
+		public GameObject mainUI;
+
+		void Start(){
+			gamePlay.SetActive (false);
+		}
+
+		public void HandlePointerClick () {
+			gamePlay.SetActive (true);
+			mainUI.SetActive (false);
+		}
+	}
+}
